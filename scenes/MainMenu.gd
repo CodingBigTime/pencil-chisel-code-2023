999d2f1d86185e56399c43f872a7e927c0cd7128
LordDeatHunter <desuhansa@gmail.com>
2023-11-11 20:57:30 +0100

    Fix world environment

--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -6,7 +6,7 @@ func _ready():
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/node_3d.tscn")
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 
 func _on_exit_button_pressed():
