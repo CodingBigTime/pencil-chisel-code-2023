9061695d46e4193a5b08b5486e35c284da770d20
Martin Popovski <martinkozle@yahoo.com>
2023-11-12 14:18:42 +0100

    Add game over scene

--- scenes/main.gd
+++ scenes/main.gd
@@ -3,6 +3,7 @@ extends Node3D
 
 func _ready():
 	$player.die.connect(go_to_main_menu)
+	$player.die.connect(GlobalState.make_game_over_visible)
 	$player.boost_count_changed.connect($main_ui/AspectRatioContainer/boost_bar.update_value)
 	$player.boost_count_changed.emit($player.boost_count)
 	$player.increase_score.connect($main_ui/AspectRatioContainer2/score_label.increase_score)
