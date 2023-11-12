5150932e5a8f7ef5ef3dfd90f25375c5bde506dc
Martin Popovski <martinkozle@yahoo.com>
2023-11-12 12:52:55 +0100

    Add score reset

--- scenes/ui/best_score_label.gd
+++ scenes/ui/best_score_label.gd
@@ -3,6 +3,11 @@ extends Label
 
 
 func _ready() -> void:
+	GlobalState.reset_best_score_signal.connect(update_best_score)
+	update_best_score()
+
+
+func update_best_score() -> void:
 	if GlobalState.new_best_score:
 		text = "New best score: " + str(GlobalState.get_best_score())
 		label_settings.font_color = Color(1.0, 1.0, 0.0)
